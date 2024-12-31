8c9c94aae3a1fa9877fa13ebc8dd5d28e7ebbf8f
mauricio-arcanjo <140445426+mauricio-arcanjo@users.noreply.github.com>
2024-12-30 23:52:02 -0500

    First commit

---
+++ src/main/java/med/voll/api/med/voll/Application.java
@@ -0,0 +1,13 @@
+package med.voll.api.med.voll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
