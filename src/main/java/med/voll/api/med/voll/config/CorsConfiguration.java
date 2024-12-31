48beea6fa8a7388965dc2fcf64907352f361d770
mauricio-arcanjo <140445426+mauricio-arcanjo@users.noreply.github.com>
2024-12-31 01:00:59 -0500

    Added comments into CorsConfiguration

--- src/main/java/med/voll/api/med/voll/config/CorsConfiguration.java
+++ src/main/java/med/voll/api/med/voll/config/CorsConfiguration.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
+    CORS is a mechanism used to add HTTP headers that tell browsers to allow a web application to run on one origin
+    and access resources from a different origin. This type of action is called a cross-origin HTTP request. In practice,
+    then, it tells browsers whether or not a particular resource can be accessed.
+
+    http://localhost:3000 would be the address of the Front-end application and .allowedMethods the methods that will
+     be allowed to be executed. With this, you can consume your API without any problems from a Front-end application.
+ */
+
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
