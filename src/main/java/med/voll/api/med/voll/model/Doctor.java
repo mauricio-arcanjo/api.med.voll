bf509c6588370085279857a4a22744590ce4973e
mauricio-arcanjo <140445426+mauricio-arcanjo@users.noreply.github.com>
2024-12-31 00:24:06 -0500

    Creation of dorctor model

---
+++ src/main/java/med/voll/api/med/voll/model/Doctor.java
@@ -0,0 +1,42 @@
+package med.voll.api.med.voll.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "doctors")
+public class Doctor {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String phone;
+
+    @NotBlank
+    private String crm;
+
+    @Enumerated (EnumType.STRING)
+    @NotBlank
+    private String speciality;
+
+}
---
+++ src/main/java/med/voll/api/med/voll/model/Speciality.java
@@ -0,0 +1,8 @@
+package med.voll.api.med.voll.model;
+
+public enum Speciality {
+    ORTHOPEDDICS,
+    CARDIOLOGY,
+    GYNECOLOGY,
+    DERMATOLOGY
+}
